76bc4d8b85c46ae600895e9c9b67230aa7d485d2
Nathanael Rachmat <19623100@std.stei.itb.ac.id>
2024-07-16 22:20:41 +0700

    fix: bug where selected chat is refreshed back

--- src/test.js
+++ src/test.js
@@ -22,22 +22,46 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// declare dbData variable as datatype=object
+// declare variable
+let selectedChat;
 let dbRef = ref(db);
+let dbSnapshot = get(dbRef);
 let dbData;
-onValue(dbRef, (snapshot) =>{
-  dbData = snapshot.val();
-  let chatIdString;
-  let chatIdHTML;
-  for (const [key,chatId] of Object.entries(dbData["user"][currentUserId]["chat"])){
+let currentUserId = window.localStorage.getItem("currentUserId");
+document.getElementById("test").innerText = `current user ID = ${currentUserId}`;
+
+//Initialize the first look (This part doesnt refresh on new item being added in database historyMessage)
+dbSnapshot.then((Snapshot) => {
+    dbData = Snapshot.val();
+    //Creates the chat list that this user have, then render it on the option list
+    let chatIdString;
+    let chatIdHTML;
+    for (const [key,chatId] of Object.entries(dbData["user"][currentUserId]["chat"])){
     if (key !== "0"){
         chatIdString = `<option value=${chatId}>${chatId}</option>`;
         chatIdHTML += chatIdString;
-    }
-  }
-  document.getElementById("chatList").innerHTML = chatIdHTML;
+    }}
+    document.getElementById("chatList").innerHTML = chatIdHTML;
 
-  let selectedChat = document.getElementById("chatList").value;
+    //Picks the selected chat, then render the history chat of the selected chat
+    selectedChat = document.getElementById("chatList").value;
+    let chatString;
+    let chatHTML = "";
+    for (const [key, values] of Object.entries(dbData["chat"][selectedChat]["historyMessage"])) {
+    if (key !== "0"){
+        chatString = `<p>
+        ${values["userID"]}:
+        ${values["content"]}
+        (${values["time"]})
+        </p>`
+        chatHTML += chatString;
+    }}
+    document.getElementById("history-message").innerHTML = chatHTML;
+    });
+
+//Refreshes the historychat everytime new item/child is added in database (historyMessage)
+onValue(dbRef, (snapshot) =>{
+  dbData = snapshot.val();
   let chatString;
   let chatHTML = "";
   for (const [key, values] of Object.entries(dbData["chat"][selectedChat]["historyMessage"])) {
@@ -53,9 +77,10 @@ onValue(dbRef, (snapshot) =>{
   document.getElementById("history-message").innerHTML = chatHTML;
 })
 
+//Everytime option of chatlist got clicked, the history chat re-rendered based on the selected chat
 document.getElementById("chatList").addEventListener("click",function(e){
     e.preventDefault;
-    let selectedChat = document.getElementById("chatList").value;
+    selectedChat = document.getElementById("chatList").value;
     let chatString;
     let chatHTML = "";
     for (const [key, values] of Object.entries(dbData["chat"][selectedChat]["historyMessage"])) {
@@ -71,21 +96,20 @@ document.getElementById("chatList").addEventListener("click",function(e){
     document.getElementById("history-message").innerHTML = chatHTML;
 })
 
-
-let currentUserId = window.localStorage.getItem("currentUserId");
-document.getElementById("test").innerText = `current user ID = ${currentUserId}`;
-
+//log out and clears currentUsedID value in localstorage
 document.getElementById("logout").addEventListener("click",function(e){
     e.preventDefault;
     window.localStorage.clear();
     window.location = "index.html";
 })
 
+//change page to addfriend
 document.getElementById("addFriend").addEventListener("click",function(e){
     e.preventDefault;
     window.location = "addFriend.html";
 })
 
+// Dont press this unless u want to reset the friends all users have to zero
 document.getElementById("resetFriend").addEventListener("click",function(e){
     e.preventDefault;
     for (const [key, values] of Object.entries(dbData["user"])) {
@@ -97,14 +121,15 @@ document.getElementById("resetFriend").addEventListener("click",function(e){
         )
     }
 })
-document.getElementById("message").addEventListener('keypress', async function(e){
+
+//Everytime 'enter' is pressed in message input text, new item (the value of input text) is added to historyMessage, this triggers onValue function above to refresh the rendering of historyMessage
+document.getElementById("message").addEventListener('keypress', function(e){
     if (e.key === 'Enter'){
         const text = document.getElementById("message").value;
-        const selectedChat = document.getElementById("chatList").value;
         const date = new Date();
         const time = `${date.getHours()}:${date.getMinutes()}`;
         let count = dbData["chat"][selectedChat]["historyMessage"].length;
-        await set(ref(db,"chat/"+selectedChat+"/"+"historyMessage/"+count),
+        set(ref(db,"chat/"+selectedChat+"/"+"historyMessage/"+count),
             {
                 "userID": currentUserId,
                 "content":text,
